e12fd1df204523d1a3d20d7c5d54295c2ed3080c
“Maggie <maggiewernet@gmail.com>
2023-02-22 19:16:36 -0500

    updated js code

--- makemoney/makemoney.js
+++ makemoney/makemoney.js
@@ -1,36 +1,18 @@
 (function () {
   console.log("hello");
-  const numberOfCoins = document.getElementById("numberOfCoins");
-  const typeOfCoins = document.getElementById("typeOfCoin");
-  const buttonMakeMoney = document.getElementById("buttonMakeMoney");
-  const coinContainer = document.getElementById("coins");
+  const makeMoneyButton = document.getElementById("buttonMakeMoney");
 
-  function makeMoney(event) {
-    event.preventDefault();
-    for(let i = 0; 1 <numberOfCoins; 1++){
-        const newCoin = document.createElement("div");
-    const newCoinP = document.createElement("p");
-    if(newCoinP.innerText = coinType.value);
-}
-newCoin.append(newCoinP);
-newCoin.addEventListener('click', (event) =>{
-    if(event.target.classList.contains('coins')){
-        event.target.remove()
-    }else{
-        event.target.parentNode.remove();
+  buttonMakeMoney.addEventListener("click", (event) => {
+    console.log(event.target);
+
+    const howMany = document.getElementById("numberOfCoins");
+    const whichCoin = document.getElementById("typeOfCoin");
+
+    for (let i = 0; i < howMany.value; i++) {
+      const newCoin = document.createElement("div");
+      newCoin.classList.add(whichCoin.value);
+
+      document.body.append(newCoin);
     }
-});
-newCoin.classList.add('coin');
-newCoin.classList.add(coinType.value);
-newContainer.append(newCoin);
-
-
-   
-    newCoinP.innerText = coinType.value;
-    newCoin.append(newCoinP);
-    newCoin.classList.add("coin");
-    coinContainer.append(newCoin);
-  }
-  
-
+  });
 })();
